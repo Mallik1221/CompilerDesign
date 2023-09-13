31ebe05c5d7850b83ddeff0b21ca0ab4c72324ac
B.K.S.S.S MALLIK <105410556+sm00th0perat0r@users.noreply.github.com>
2023-09-13 16:33:25 +0530

    Add files via upload

---
+++ AST2/program.py
@@ -0,0 +1,22 @@
+class Program:
+	def __init__(self):
+		self.functions = {}		
+	
+	def addFunctionDetails(self,name, function):
+		self.functions[name] = function
+
+	def print(self):
+		for funname, function in self.functions.items():
+			# print(f"Function Name: {funname}")
+			function.print()
+	
+
+	def getMainFunction(self):
+		for funname, function in self.functions.items():
+			if(funname == 'main'):
+				return function
+    
+	# def getFunctionDeatils(self, name):
+	# 	if name in functions:
+	# 		return functions[name]
+	# 
